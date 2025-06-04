fe96b7aee47d0a8a0c4c8722a06c0cc653b8e9c7
Ayush Samad <maxala7@gmail.com>
2025-06-05 01:22:10 +0530

    20 first try

---
+++ 20.Valid_Parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        hashmap = {"(":[")",0],"{":["}",1],"[":["]",2]}
+        latest_bracket = []
+        for char in s:
+            for bracket in hashmap.keys():
+                if char == bracket:
+                    latest_bracket.append(hashmap[bracket][1])
+                elif char == hashmap[bracket][0]:
+                    try:
+                        if hashmap[bracket][1] != latest_bracket.pop():
+                            return False
+                    except:
+                        return False
+                    
+        if latest_bracket:
+            return False
+        return True 
+
+Solution = Solution()
+print(Solution.isValid("(("))
