6b5d7c9d24dac8e4f70d77089c70bf2dbe135c29
Ayush Samad <maxala7@gmail.com>
2025-09-03 03:05:23 +0530

    5th done in a WORSE way

--- 5. Longest Palindromic Substring.py
+++ 5. Longest Palindromic Substring.py
@@ -1,17 +1,25 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         # c=0
-        ls = [0,0]
-        for i in range(0,len(s)):
-            for j in range(i+1+ls[1]-ls[0],len(s)+1):
+        if len(s)==1:
+            return s
+        for i in range(len(s),0,-1):
+            # print(i)
+            j=0
+            while (i+j<=len(s)):
+                # print(i,j,i+j,s[j:i+j],s[i+j-1:None if j == 0 else j-1:-1])
+                if s[j:i+j] == s[i+j-1:None if j == 0 else j-1:-1]:
+                    return s[j:i+j]
+                j+=1
+            # for j in range():
                 # print(s[i:j])
                 # c+=1
-                if s[i:j]==s[j-1:None if i == 0 else i-1:-1]:
-                    if ls[1]-ls[0] < j-i:
-                        ls = [i,j]
+                # if s[i:j]==s[j-1:None if i == 0 else i-1:-1]:
+                #     if ls[1]-ls[0] < j-i:
+                #         ls = [i,j]
         
         # print (c)
-        return s[ls[0]:ls[1]]
+        # return s[ls[0]:ls[1]]
 
 sol = Solution()
-print (sol.longestPalindrome("babad"))
+print (sol.longestPalindrome("bb"))
